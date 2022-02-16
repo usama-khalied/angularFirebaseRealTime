871a562e3b2ca0cd98b02f4d0d410a92ac26bff8
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-02-16 10:50:41 +0500

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBwbfvSd8v8jOQ3iRW2qDY-48tBHWjZDek',
+    authDomain: 'angular-01-9faf4.firebaseapp.com',
+    projectId: 'angular-01-9faf4',
+    storageBucket: 'angular-01-9faf4.appspot.com',
+    messagingSenderId: '893148326142',
+    appId: '1:893148326142:web:bffe96abe0396050b49daf',
+  },
+};
